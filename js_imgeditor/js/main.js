9e4cc81f697e95ce135bf2fa48db84be0acc293e
unknown <sys970717@naver.com>
2018-08-30 18:51:14 +0900

    마우스 커서로 div box 이동 구현 완료. TODO : box resize dot 및 기능 구현

--- js_imgeditor/js/main.js
+++ js_imgeditor/js/main.js
@@ -1,18 +1,20 @@
 var IMAGE_EDITOR = IMAGE_EDITOR || {};
+var mouseX = 0, mouseY = 0; // Mouse Event Position
+var posX = 0, posY = 0; // div Box Position
 
-var toggleBox = function(elementId, condition) {
-    switch(condition) {
-        case "open" : 
-            $("#"+elementId).removeClass('close');
+var toggleBox = function (elementId, condition) {
+    switch (condition) {
+        case "open":
+            $("#" + elementId).removeClass('close');
             break;
-        case "close" : 
-            $("#"+elementId).addClass('close');
+        case "close":
+            $("#" + elementId).addClass('close');
             break;
     }
 }
 
-var manageState = function(eventName) {
-    if(IMAGE_EDITOR.state.condition === true){
+var manageState = function (eventName) {
+    if (IMAGE_EDITOR.state.condition === true) {
         return;
     } else {
         IMAGE_EDITOR.state.condition = true;
@@ -20,59 +22,58 @@ var manageState = function(eventName) {
     }
 }
 
+var elementDrag = function(e) {
+    posX = mouseX - e.clientX;
+    posY = mouseY - e.clientY;
+
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+
+    $("#toolBox.crop_box").css({
+        // if using offset, don't behavior you wants
+        top: ($("#toolBox.crop_box").position().top-posY),
+        left: ($("#toolBox.crop_box").position().left-posX)
+    });
+}
+
 IMAGE_EDITOR.events = {
-    crop : {
-        open : function() {
-            if(IMAGE_EDITOR.state.condition) {
+    crop: {
+        open: function () {
+            if (IMAGE_EDITOR.state.condition) {
                 alert("잘라내기 기능을 사용중입니다.");
                 return;
             } else {
                 $("#toolBox").addClass('crop_box');
+                $("#toolBox").resizable();
                 toggleBox('toolBox', 'open');
                 manageState("crop");
-                document.getElementById('toolBox').onmousedown = this.move;
-            }
-        },
-        move : function(e) {
-            e = e || widnow.event;
-            e.preventDefault();
-           
-            if(e.type === "mousedown") {
-                $(IMAGE_EDITOR.object.getBoxObj()).on("mousemove", function(e) {
-                    e = window.event;
-                    e.preventDefault();
-                    if(e.clientX < $(IMAGE_EDITOR.object.getBoxObj()).offset().left) {
+                
+                $("#toolBox").on("mousedown", function() {
+                    toggleBox('div_view', 'open');
+                    mouseX = event.clientX;
+                    mouseY = event.clientY;
 
-                    } else if(e.clientX > $(IMAGE_EDITOR.object.getBoxObj()).prop('width')) {
-
-                    } else if(e.clientY < $(IMAGE_EDITOR.object.getBoxObj()).offset().top) {
-
-                    } else if(e.clientY > $(IMAGE_EDITOR.object.getBoxObj()).prop('height')) {
-                        
-                    }
-
-                    console.log("CLIENT : " +e.clientX + " \t "+e.clientY);
-                    console.log("PAGE : " +e.pageX + " \t "+e.pageY);
-
-                    $(".crop_box").attr("style", "top : "+(e.pageY)+"px; left : "+(e.pageX)+"px;");
+                    $(IMAGE_EDITOR.object.getBoxObj()).on("mousemove", elementDrag);
                 });
 
-                $(document).on("mouseup", function() {
+                $(document).on("mouseup", function () {
+                    toggleBox('div_view', 'close');
+                    $("#toolBox").off("mouseup", this);
                     document.getElementById("toolBox").onmouseup = null;
                     // all event stop
-                    $(IMAGE_EDITOR.object.getBoxObj()).off("mousemove", this.move);
+                    $(IMAGE_EDITOR.object.getBoxObj()).off("mousemove", elementDrag);
                 });
             }
         },
 
-        cut : function() {
+        cut: function () {
 
         },
-        cancel : function() {
+        cancel: function () {
 
         }
     },
-    handleDrop : function(files){
+    handleDrop: function (files) {
         // Multiple files can be dropped. Lets only deal with the "first" one.
         var file = files[0];
         // Image check
@@ -84,20 +85,20 @@ IMAGE_EDITOR.events = {
                 alert("Error : Undefined Canvas tag \n Solution : Check definition canvas tag on your html");
                 return;
             }
-            
+
         } else {
             alert("That file wasn't an image.");
         }
     },
 
-    __fileUpload : function(file, size) {
+    __fileUpload: function (file, size) {
         var fileReader = new FileReader;
-        fileReader.onload = function() {
+        fileReader.onload = function () {
             var image = new Image();
-            image.onload = function(){
+            image.onload = function () {
                 var canvas = document.getElementById("canvas");
-               
-                if(image.width > size) {
+
+                if (image.width > size) {
                     image.height *= size / image.width;
                     image.width = size;
                 }
@@ -107,10 +108,10 @@ IMAGE_EDITOR.events = {
                 canvas.height = image.height;
 
                 $("#div_view").css({
-                    width : canvas.width+'px',
-                    height : canvas.height+'px',
-                    top : canvas.offsetTop+'px',
-                    left : canvas.offsetLeft+'px'
+                    width: canvas.width + 'px',
+                    height: canvas.height + 'px',
+                    top: canvas.offsetTop + 'px',
+                    left: canvas.offsetLeft + 'px'
                 });
 
                 ctx.drawImage(image, 0, 0, image.width, image.height);
@@ -123,52 +124,52 @@ IMAGE_EDITOR.events = {
 
 // 에디터 기능 상태관리 ( 다른 툴을 이용하여 편집 중 충돌이 생기지 않도록 한다. )
 IMAGE_EDITOR.state = {
-    condition : false,
-    stateLog : [],
-    setStateLog : function(eventName) {
-        if(this.stateLog.length >= 20) {
+    condition: false,
+    stateLog: [],
+    setStateLog: function (eventName) {
+        if (this.stateLog.length >= 20) {
             //TODO : 배열 한칸씩 오른쪽으로 밀기
         } else {
             //TODO : History 담기
             this.stateLog.push(eventName);
         }
     },
-    getStateLog : function() {
+    getStateLog: function () {
         return this.stateLog.Array.prototype.slice(0);
     }
 };
 
 IMAGE_EDITOR.object = {
-    __object : "",
-    setObject : function(elementId) {
+    __object: "",
+    setObject: function (elementId) {
         this.__object = document.getElementById(elementId) === undefined ? document.getElementsByTagName('canvas')[0] : document.getElementById(elementId);
     },
 
-    getObject : function() {
+    getObject: function () {
         return this.__object;
     },
 
-    getWidth : function() {
+    getWidth: function () {
         return this.__object.getAttribute("width") === undefined || this.__object.getAttribute("width") < 200 ? 500 : this.__object.getAttribute("width");
     },
 
-    getHeight : function() {
+    getHeight: function () {
         return this.__object.getAttribute("height") === undefined ? 500 : this.__object.getAttribute("height");
     },
 
-    getBoxObj : function() {
+    getBoxObj: function () {
         return document.getElementById('div_view');
     }
 }
 
-var init = function(elementId) {
+var init = function (elementId) {
     IMAGE_EDITOR.object.setObject(elementId);
 
-    $("#fileUpload").on("change", function(event) {
+    $("#fileUpload").on("change", function (event) {
         IMAGE_EDITOR.events.handleDrop(event.target.files)
     });
-    
-    $("#crop_open").on("click", function() {
+
+    $("#crop_open").on("click", function () {
         IMAGE_EDITOR.events.crop.open();
     });
 }
